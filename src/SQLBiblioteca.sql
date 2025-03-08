a8f860f301b69af50554f74a32a05443f69d3395
Julian Preiti <58276522+julianpreiti@users.noreply.github.com>
2025-03-08 17:32:54 -0300

    sql commit

---
+++ src/SQLBiblioteca.sql
@@ -0,0 +1,75 @@
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema SistemaBiblioteca
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `SistemaBiblioteca` ;
+
+-- -----------------------------------------------------
+-- Schema SistemaBiblioteca
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `SistemaBiblioteca` ;
+USE `SistemaBiblioteca` ;
+
+-- -----------------------------------------------------
+-- Table `SistemaBiblioteca`.`usuario`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SistemaBiblioteca`.`usuario` ;
+
+CREATE TABLE IF NOT EXISTS `SistemaBiblioteca`.`usuario` (
+  `idusuario` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(150) NULL,
+  `dni` INT NULL,
+  `email` VARCHAR(255) NULL,
+  PRIMARY KEY (`idusuario`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SistemaBiblioteca`.`libro`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SistemaBiblioteca`.`libro` ;
+
+CREATE TABLE IF NOT EXISTS `SistemaBiblioteca`.`libro` (
+  `idlibro` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(150) NULL,
+  `autor` VARCHAR(150) NULL,
+  `categoria` VARCHAR(150) NULL,
+  `disponible` TINYINT NULL,
+  PRIMARY KEY (`idlibro`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SistemaBiblioteca`.`usuario_prestamo`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SistemaBiblioteca`.`usuario_prestamo` ;
+
+CREATE TABLE IF NOT EXISTS `SistemaBiblioteca`.`usuario_prestamo` (
+  `idPrestamo` INT NOT NULL AUTO_INCREMENT,
+  `idusuario` INT NOT NULL,
+  `idlibro` INT NOT NULL,
+  `fechaPrestamo` DATE NULL,
+  `fechaDevolucion` VARCHAR(45) NULL,
+  PRIMARY KEY (`idPrestamo`, `idusuario`, `idlibro`),
+  INDEX `fk_usuario_has_libro_libro1_idx` (`idlibro` ASC) VISIBLE,
+  INDEX `fk_usuario_has_libro_usuario_idx` (`idusuario` ASC) VISIBLE,
+  CONSTRAINT `fk_usuario_has_libro_usuario`
+    FOREIGN KEY (`idusuario`)
+    REFERENCES `SistemaBiblioteca`.`usuario` (`idusuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuario_has_libro_libro1`
+    FOREIGN KEY (`idlibro`)
+    REFERENCES `SistemaBiblioteca`.`libro` (`idlibro`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
