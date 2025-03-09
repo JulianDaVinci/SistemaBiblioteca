4cff120dbd34faa99a33a13a7a04ab5bba5e3011
Julian Preiti <58276522+julianpreiti@users.noreply.github.com>
2025-03-09 06:40:53 -0300

    sql commit 2.6

--- src/Modelo/Main.java
+++ src/Modelo/Main.java
@@ -2,7 +2,7 @@ package Modelo;
 import javax.swing.JFrame;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main() {
         Menu menu = new Menu();
         menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         menu.setVisible(true);
